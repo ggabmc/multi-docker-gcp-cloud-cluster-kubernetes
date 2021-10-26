2befd63577fdb9e52c49dd81beaa997e186582a5
Gabriel <ggabmc@hotmail.com>
2021-10-25 21:50:57 -0700

    added projectid, zone and cluster in yaml file

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build --tag ggabmc/multi-client:latest --tag ggabmc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build --tag ggabmc/multi-server:latest --tag ggabmc/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build --tag ggabmc/multi-worker:latest --tag ggabmc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push ggabmc/multi-client:latest
+docker push ggabmc/multi-server:latest
+docker push ggabmc/multi-worker:latest
+
+docker push ggabmc/multi-client:$SHA
+docker push ggabmc/multi-server:$SHA
+docker push ggabmc/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=ggabmc/multi-client:$SHA
+kubectl set image deployments/server-deployment server=ggabmc/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=ggabmc/multi-worker:$SHA
+
